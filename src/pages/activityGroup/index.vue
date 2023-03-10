40ebb769ee6be1d0a95b657f248467aafa24ced6
zhengwei111 <1217967340@qq.com>
2023-03-10 17:36:35 +0800

    1

---
+++ src/pages/activityGroup/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <uni-container> 123 </uni-container>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import type { ActivityGroup } from '@/typings/activity'
+
+const activityGroups = reactive<ActivityGroup[]>([])
+</script>
